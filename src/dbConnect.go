80ff022a274e15ef2917c67e1ae8ce36a1fb58ac
Adriel da Silva <silvaadriel510@gmail.com>
2019-09-30 15:04:06 -0300

    dbConnect created

---
+++ src/dbConnect.go
@@ -0,0 +1,11 @@
+package main
+
+import "database/sql"
+
+// DBConnect opens a postgres database connection
+func DBConnect() (*sql.DB, error) {
+	connStr := "dbname=crud user=postgres password=postgres sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+
+	return db, err
+}
