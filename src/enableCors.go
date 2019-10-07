9943c2d0c9171af3e1b40b3dccc5fab31ba428c3
Adriel da Silva <silvaadriel510@gmail.com>
2019-10-06 22:16:05 -0300

    Add more information to the header

--- src/enableCors.go
+++ src/enableCors.go
@@ -4,4 +4,8 @@ import "net/http"
 
 func enableCors(responseWriter *http.ResponseWriter) {
 	(*responseWriter).Header().Set("Access-Control-Allow-Origin", "*")
+	(*responseWriter).Header().Set("Access-Control-Allow-Methods",
+		"POST, GET, OPTIONS, PUT, DELETE")
+	(*responseWriter).Header().Set("Access-Control-Allow-Headers",
+		"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
